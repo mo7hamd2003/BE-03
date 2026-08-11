691cd2bf48d9375e3825a90db02dcf324cfe56d2
mo7hamd2003 <mohmdsteitieh123@gmail.com>
2026-08-11 14:53:41 +0300

    Stage 0: setup server & supbase client

---
+++ main.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine 
+from dotenv import load_dotenv
+import os
+
+# Stage-0: Load .env variables
+load_dotenv()
+
+# Stage-0: Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+# Stage-0: Construct the SQLAlchemy connection string
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+# Stage-0: Create SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+
+# Stage-0: Try to connect
+try:
+    with engine.connect() as connection:
+        print("Server running and connected to Supabase")
+except Exception as e:
+    print(f"Failed to connect: {e}")
+
